28ea3251c4091cf6d867ae3b236546cc9fc9ffba
shatlyk-jr <mr.liar.2001@gmail.com>
2023-05-04 04:07:25 +0500

    bg-service added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,48 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:workmanager/workmanager.dart';
+
 import 'import.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+  Workmanager().registerPeriodicTask("2", "Simple task",
+      frequency: const Duration(minutes: 15));
+
   runApp(const MyApp());
+}
+
+void callbackDispatcher() {
+  Workmanager().executeTask((taskName, inputData) {
+    FlutterLocalNotificationsPlugin flip = FlutterLocalNotificationsPlugin();
+    var android = const AndroidInitializationSettings("@mipmap/ic_launcher");
+
+    var settings = InitializationSettings(android: android);
+    flip.initialize(settings);
+    _showNotifWithDefSound(flip);
+    return Future.value(true);
+  });
+}
+
+Future _showNotifWithDefSound(flip) async {
+  var androidPlatformChannelSpec = const AndroidNotificationDetails(
+    "my id",
+    "channelName",
+    channelDescription: "description",
+    importance: Importance.max,
+    priority: Priority.high,
+  );
+
+  var platformChannelSpec =
+      NotificationDetails(android: androidPlatformChannelSpec);
+  await flip.show(
+    0,
+    "Coding is meaningful",
+    platformChannelSpec,
+    payload: "Default Sound",
+  );
 }
 
 class MyApp extends StatelessWidget {
